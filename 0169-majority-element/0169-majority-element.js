6a211647e1c3c02814724243a47e43e17731da68
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-03 23:16:32 +0600

    Time: 86 ms (81.74%), Space: 43.2 MB (88.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,17 +3,14 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-   let frequency={}
-   let count=0;
-   for(i=0;i<nums.length;i++){
-      if(frequency[nums[i]]==undefined){
-        frequency[nums[i]]=1       
-}
-      else{
-    frequency[nums[i]]+=1  
-}
-       if(frequency[nums[i]]>nums.length/2){
-    return nums[i];
-   }
-  }
+    let count = 0, maxValue = 0;
+    
+    for(let i=0; i < nums.length; i++){
+        if(count === 0){
+            maxValue = nums[i];
+        }
+        count += nums[i] === maxValue ? 1 : -1;
+    }
+    
+    return maxValue;
 };
