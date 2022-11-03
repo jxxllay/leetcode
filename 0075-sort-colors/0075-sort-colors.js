1ff054f915526f01bfe17d7a9feb2f90dddc09d1
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-03 22:08:04 +0600

    Time: 75 ms (82.96%), Space: 42.4 MB (28.55%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+for(let i = 0;i<nums.length;i++){
+for(let j = 0; j <nums.length-1-i;j++){
+      if(nums[j]>nums[j+1]){
+         [nums[j],nums[j+1]]=[nums[j+1],nums[j]]
+        }
+    }
+}
+    return nums
+  }
