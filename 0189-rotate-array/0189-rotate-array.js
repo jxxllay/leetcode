7c97f91417e493bea7d537c5cadc622a54a55e59
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-04 20:04:56 +0600

    Time: 155 ms (69.94%), Space: 51 MB (90.74%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    let rotate = nums.splice(nums.length - k, k);
+    nums.unshift(...rotate);
+};
