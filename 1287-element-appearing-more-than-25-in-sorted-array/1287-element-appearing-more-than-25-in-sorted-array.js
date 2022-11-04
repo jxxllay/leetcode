ae6f689ed063253a93d724ba822d549749e2e598
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-04 21:07:07 +0600

    Time: 122 ms (32.07%), Space: 46.9 MB (25.32%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -2,4 +2,14 @@
  * @param {number[]} arr
  * @return {number}
  */
-const findSpecialInteger = A => A.filter(n => ((A.lastIndexOf(n) - A.indexOf(n) + 1) / (A.length + 1) >= 0.25))[0]
+
+
+var findSpecialInteger = function(arr) {
+  let map = new Map();
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (map.has(arr[i])) map.set(arr[i], map.get(arr[i]) + 1);
+    if (!map.has(arr[i])) map.set(arr[i], 1);
+  }
+  return [...map][[...map.values()].indexOf(Math.max(...map.values()))][0];
+}; 
