1473ddf9b63babe622057660bfbd38f96979207c
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-05 20:14:16 +0600

    Time: 323 ms (78.49%), Space: 70.1 MB (54.34%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+  const map = new Map();
+  [...new Set(arr)].sort((a, b) => a - b).forEach((x, i) => map.set(x, i + 1));
+  return arr.map(x => map.get(x));
+};
