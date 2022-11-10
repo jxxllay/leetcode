a4595df8d9a2cfc000fe74976f31fbad5315ac75
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-10 22:55:35 +0600

    Time: 100 ms (86.77%), Space: 44.8 MB (84.82%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.js
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -3,17 +3,17 @@
  * @return {string[]}
  */
 var commonChars = function(words) {
-    let word =words[0].split('');
+    let letters =words[0].split('');
     let arr =[];
     for(i=0;i<words.length;i++){
         for(j=0;j<=words[i].length;j++){
-            if(word.includes(words[i].charAt(j))){
+            if(letters.includes(words[i].charAt(j))){
                 arr.push(words[i].charAt(j));
-                word.splice(word.indexOf(words[i].charAt(j)),1);
+                letters.splice(letters.indexOf(words[i].charAt(j)),1);
             }
         }
-        word=arr;
+        letters=arr;
         arr=[];
     }
-    return word;
+    return letters;
 };
