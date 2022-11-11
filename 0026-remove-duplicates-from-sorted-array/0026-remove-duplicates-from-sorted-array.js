510c86a5c98b71ba87a44c56886cf85c8deb8e2f
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-11 20:59:04 +0600

    Time: 120 ms (64.95%), Space: 44.8 MB (47.95%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== nums[i+1]){
+            nums[k++] = nums[i];
+        }
+    }
+    return k;
+};
