590b31c611797c9eed8b87f2e1a37547c98be5d3
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-17 23:46:56 +0600

    Time: 306 ms (71.60%), Space: 62.5 MB (62.35%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.js
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.js
@@ -3,12 +3,11 @@
  * @return {string}
  */
 var makeFancyString = function(s) {
-     let ans = ''
-          for (let i = 0; i < s.length; i++) {
-          while (s[i] === s[i + 1] && s[i] === s[i + 2]) {
-      i++
-    } 
-      ans += s[i]
-  }
-  return ans
+    let leng = s.length;
+    let res = '';
+        for (let i = 0; i < leng; i++) {
+        if (i - 2 >= 0 && s[i - 1] == s[i - 2] && s[i] == s[i - 1]) continue;
+        res += s[i];
+    }
+    return res;
 };
