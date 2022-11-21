88b581e9dd0483bdf4d7c387c7e2afc857b820bf
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-21 19:37:45 +0600

    Time: 115 ms (6.16%), Space: 42.1 MB (22.46%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
@@ -3,15 +3,15 @@
  * @return {boolean}
  */
 var squareIsWhite = function(coordinates) {
-    const arr = {};
+    const map = {};
     let flag = false;
         for (let i = 1; i <= 8; i++) {
             let char = String.fromCharCode(i + 96);
         for (let j = 1; j <= 8; j++) {
-            arr[char + j] = flag;
+            map[char + j] = flag;
             flag = !flag;
         }
         flag = !flag; 
     }
-    return arr[coordinates];
+    return map[coordinates];
 };
