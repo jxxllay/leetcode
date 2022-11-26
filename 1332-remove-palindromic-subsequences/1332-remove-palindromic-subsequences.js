5e6587acd6ec8f1408005c89e608b30cec68c95e
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-27 00:00:06 +0600

    Time: 66 ms (88.43%), Space: 42.2 MB (27.69%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function(s) {
+     if(s=='')
+         return 0
+     else if (s===s.split('').reverse().join(''))   
+         return 1
+     else 
+         return 2
+};
