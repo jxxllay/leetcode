671287e708187710fa12966370956c1cb2e18cb5
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-28 21:16:19 +0600

    Time: 669 ms (70.98%), Space: 109 MB (66.08%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -4,16 +4,16 @@
  */
 var findWinners = function(matches) {
     let winners = new Set();                
-    let losers = new Map();               
+    let loses = new Map();               
          for (let [i, k] of matches) {
               winners.add(i);
               winners.add(k);
-              losers.set(k, (losers.get(k) || 0) + 1);
+              loses.set(k, (loses.get(k) || 0) + 1);
         }
     let res = [[], []];              
          for (let x of winners) {
-             if (losers.has(x) === false) res[0].push(x);
-             if (losers.get(x) === 1)     res[1].push(x);
+             if (loses.has(x) === false) res[0].push(x);
+             if (loses.get(x) === 1)     res[1].push(x);
     }
     return res.map((arr) => arr.sort((a, b) => a - b));
 };
