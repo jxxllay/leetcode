9f0803a4f2a2537bd3d890c60f3c67ca66df17c7
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-29 22:13:44 +0600

    Time: 70 ms (84.91%), Space: 42.1 MB (29.78%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+    let first = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
+    let second = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+    let third = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+    let res = [];
+    
+    words.forEach(x => {
+        let split = x.toLowerCase().split('');
+        if(split.every(j => first.includes(j))) res.push(x)
+        if(split.every(j => second.includes(j))) res.push(x)
+        if(split.every(j => third.includes(j))) res.push(x)
+    })
+    return res;
+};
