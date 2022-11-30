d6fe8b352cf3cb0b7ace6fe451229de880370a39
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-30 23:13:02 +0600

    Time: 100 ms (44.27%), Space: 43.8 MB (19.83%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -6,16 +6,16 @@ var uniqueOccurrences = function(arr) {
     let res = {}
         if((Array.from(new Set(arr)).length === arr.length)){
             return false
-    }
+         }
         for(i of arr){
             res[i] ? (res[i] +=1) : res[i] = 1
-    }
+         }
     let outlet = []
         for(i in res){
         if(outlet.includes(res[i])){
             return false
-    }
+         }
         outlet.push(res[i])
-}
+    }
     return res
 };
