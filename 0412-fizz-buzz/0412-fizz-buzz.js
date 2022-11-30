b777d3eaf08da7b501216878d171bf29e8fee5df
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-11-30 23:23:27 +0600

    Time: 109 ms (51.03%), Space: 44.3 MB (69.13%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,16 +3,12 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-       const res = [];
-            for (let i = 1; i < n+1; i++) {
-              if ((i % 3 === 0) && (i % 5 === 0)) {
-                res.push('FizzBuzz');
-            } else if (i % 3 === 0) {
-                res.push('Fizz');
-            } else if (i % 5 === 0) {
-                res.push('Buzz');
-            } else 
-                res.push(i.toString());
+        const res = new Array(n);
+          for (let i = 1; i <= n ; i++){
+          if(i % 15 === 0) res[i-1] = 'FizzBuzz';
+          else if(i % 3 === 0) res[i-1] = 'Fizz';
+          else if(i % 5 === 0) res[i-1] = 'Buzz';
+          else res[i-1] = i.toString();
     }
-     return res;
+    return res;
 };
