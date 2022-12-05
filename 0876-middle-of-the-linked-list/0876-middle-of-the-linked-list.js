ad38319ea0c7aa15a79bb3e766ac0d8af515b52e
Isakova Aizhan <isakova.aizhan1208@gmail.com>
2022-12-06 00:19:05 +0600

    Time: 102 ms (26.64%), Space: 41.9 MB (72.13%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let res = head, one = head;
+      while (one !== null) {
+        one = one.next;
+      if (one == null) break;
+        else one = one.next;
+        res = res.next;
+    }
+    return res;
+};
